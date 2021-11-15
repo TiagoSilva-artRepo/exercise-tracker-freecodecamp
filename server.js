93c86b3495aa0ff19dc8b0a16fa569dcf1ffe65f
Tiago Silva <tiago.silva@contisystems.pt>
2021-11-15 12:04:04 +0000

    add post user endpoint

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+var mongoose = require('mongoose');
+const { response } = require('express');
+const Schema = mongoose.Schema;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +17,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = new Schema({
+  username: { type: String, required: true }
+});
 
+const User = mongoose.model("User", userSchema);
 
+app.post('/api/users', function (req, res) {
 
+  if (!req.body.username || req.body.username.length === 0) {
+    return res.status(400).json({error : "Required field username."});
+  }
+  
+  try {    
+    const user = new User({
+      username: req.body.username
+    });
+  
+    user.save();
+  
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
